6b799ce8f137020a9dbb7bd52b33994500d5f4cb
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-01-19 20:47:11 -0800

    first almost working task 0. Missing error printing and styling

--- monty.c
+++ monty.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "monty.h"
-
 #include <sys/types.h>                                         
 #include <sys/stat.h>                                          
 #include <fcntl.h>
@@ -19,13 +18,7 @@ int main(int argc, char *argv[])
 	int index  = 0;
 	int pre_index = 0;
 	char *token;
-	/*
-	char *command;
-	char *number;
-	int n_i;
-	*/
 	stack_t *head  = malloc(sizeof(stack_t));
-	
 
 	line[0] = ' ';
 	if (head == NULL)
@@ -35,78 +28,49 @@ int main(int argc, char *argv[])
 	head->n = -1;
 	head->prev = NULL;
 	head->next = NULL;
-
 	if (buff == NULL)
 	{
-		printf("couldn't allocate mem for buf\n");
-	}
+		printf("couldn't allocate mem for buf\n"); }
 	if (argc == 2)
 	{
-		filename = argv[1];
-	}
-
+		filename = argv[1]; }
 	else{
-		printf("USAGE: monty file\n");
-	}
-	
-
+		printf("USAGE: monty file\n"); }
 	if (filename == NULL)
 	{
-		return 0;
-	}
-
-
-
+		return 0; }
 	fd = open(filename, O_RDONLY);                     
-	
 	if (fd == -1)
 	{
 		printf("Error: Can't open file %s\n", filename);
-		return 1;
-	}
+		return 1; }
 	n  = read(fd, buff, 1024);
-
 	while(n!= 0)
 	{
 		n  = read(fd, buff, 1024);
-		printf("\n\n|new file %s \nLine: ",filename);
 		while( buff[index]!= '\0')
 		{
 			if (buff[index] == '\n')
 			{
-				printf("\nLine: ");
 				line[index+1] = '\0';
 				token  = strtok(line, " ");
-				printf("token at %d: %s, size: %d\n",index, token, index - pre_index);
 				if (strcmp(token,"push") == 0)
 				{
 					token = strtok(NULL," ");
-					printf("parameter 2: %s, as int %d\n", token, atoi(token));
 					push(head, atoi(token));
 				}	
 				else if (strcmp(token,"pall") == 0)
 				{
 					pall(head);
 				}
-				/*
-				 * push(head,index);
-				 */
 				pre_index = index;
-				
 			}
-			else
-			{
-				printf("%c",'\t');
-				
-			}
-			index += 1;
+			index+=1;
 			line[index-pre_index] = buff[index];
 		}
 	}
-	pall(head);
 	close(fd);
 	free(buff);
-
-
+	free(line);
 	exit(EXIT_SUCCESS);
 }
--- monty.h
+++ monty.h
@@ -11,9 +11,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 /**
  * struct instruction_s - opcode and its function
@@ -25,8 +25,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 int push(stack_t *head, int n);
