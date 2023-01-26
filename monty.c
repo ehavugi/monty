2ba0a3e61f1e1393938d961f2684516941b8c639
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-01-25 18:29:21 -0800

    fixed push -0 error and large file fix attempt 1

--- monty.c
+++ monty.c
@@ -11,6 +11,36 @@
 #define BUFFER_SIZE 1024
 
 /**
+ * numeric - check if number is numeric
+ * @str: string to check
+ * Return: -1 if not numeric and positve number if num
+ */
+int numeric(char *str)
+{
+	int index = 0;
+	int len = 0;
+
+	if (str == NULL)
+	{
+		return (-1);
+	}
+	len = strlen(str);
+	while (index < len)
+	{
+		if ((str[index] >= '0' && str[index] <= '9') || str[index] == '-')
+		{
+			index += 1;
+		}
+		else
+		{
+			printf("%s  , %c", str, str[index]);
+			return (-1);
+		}
+	}
+	return (index);
+}
+
+/**
  * get_token - extract tokens
  * @line: line to consider
  * @head: stack to process
@@ -28,7 +58,7 @@ int get_token(char *line, stack_t *head, int line_index)
 	if (strcmp(token, "push") == 0)
 	{
 		token = strtok(NULL, " ");
-		if (token == NULL || (atoi(token) == 0 && strcmp(token, "0") != 0))
+		if (token == NULL || numeric(token) < 0)
 		{
 			fprintf(stderr, "L%d: usage: push integer\n", line_index);
 			return (EXIT_FAILURE);
@@ -69,23 +99,22 @@ int get_token(char *line, stack_t *head, int line_index)
 int process_file(const char *filename, stack_t *head, char *buff)
 {
 	int fd;
+	int offset = 0;
 	ssize_t n;
 	char *line;
 	int index = 0;
 	int line_index = 1;
 	int line_char = 0;
+	int last_index = 0;
 
 	line  = malloc(BUFFER_SIZE);
-	line[0] = ' ';
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", filename);
-		return (EXIT_FAILURE);
-	}
+		return (EXIT_FAILURE); }
 	n = read(fd, buff, BUFFER_SIZE);
-	while (n != 0)
+	while (n > 0)
 	{
 		n = read(fd, buff, BUFFER_SIZE);
 		while (buff[index] != '\0' && index < BUFFER_SIZE)
@@ -95,18 +124,19 @@ int process_file(const char *filename, stack_t *head, char *buff)
 				line[line_char] = '\0';
 				if (get_token(line, head, line_index) == EXIT_FAILURE)
 				{
-					return (EXIT_FAILURE);
-				}
+					return (EXIT_FAILURE); }
 				line_index += 1;
 				line_char = 0;
-			}
+				last_index = index; }
 			else
 			{
 				line[line_char] = buff[index];
 				line_char += 1; }
 			index += 1; }
-	}
+		offset += last_index;
+		lseek(fd, offset, SEEK_SET); }
 	free(line);
+	close(fd);
 	return (EXIT_SUCCESS);
 }
 
